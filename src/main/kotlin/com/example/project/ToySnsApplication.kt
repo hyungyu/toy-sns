422094dba25d02e604eb5752330dca8d6295fe4e
Eden <phkhyun@naver.com>
2023-06-22 15:13:37 +0900

    first commit

---
+++ src/main/kotlin/com/example/project/ToySnsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToySnsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ToySnsApplication>(*args)
+}
